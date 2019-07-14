fc0ffd9d23966ff4eb4b669717168d1a40af1447
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-14 14:24:15 +0545

    Main Game logic

--- game.js
+++ game.js
@@ -13,3 +13,48 @@ const squareCount = 9;
 const squares = document.getElementsByClassName('square');
 let difficulty = 'beginner';
 let gameOver = false;
+
+const setMessageBox = caption => {};
+
+const checkForWinCondition = marker => {};
+
+const opponentMove = () => {};
+
+const squareIsOpen = square => {};
+
+const checkForDraw = () => {};
+
+// Game Logic
+const chooseSquare = () => {
+  difficulty = document.getElementById('difficulty').value;
+  if (!gameOver) {
+    setMessageBox('Pick a square!');
+    const id = this.getAttribute('id');
+    const square = document.getElementById(id);
+    if (squareIsOpen(square)) {
+      square.innerHTML = 'X';
+      const win = checkForWinCondition('X');
+      if (!win) {
+        opponentMove();
+        const lost = checkForWinCondition('O');
+        if (!lost) {
+          const draw = checkForDraw();
+          if (draw) {
+            gameOver = true;
+            setMessageBox("It's a draw!");
+          }
+        } else {
+          gameOver = true;
+          highlightWinningSquares(lost, 'rgb(42,178,72)');
+          setMessageBox('You lost!');
+        }
+      } else {
+        gameOver = true;
+        highlightWinningSquares(win, 'rgb(42,178,72)');
+        setMessageBox('You won!');
+      }
+    } else {
+      setMessageBox('That square is already taken!');
+    }
+  }
+};
